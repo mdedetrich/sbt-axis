a43995997d01a35a09841c366f9c8d3e22c7f2b1
Matthew de Detrich <mdedetrich@gmail.com>
2021-08-02 13:59:06 +0200

    Apply scalafmt

--- src/main/scala/sbtaxis/Plugin.scala
+++ src/main/scala/sbtaxis/Plugin.scala
@@ -18,10 +18,13 @@ object AxisPlugin extends sbt.AutoPlugin {
 
     val axisWsdl2java = TaskKey[Seq[File]]("wsdl2java", "Runs WSDL2Java")
     val axisWsdlFiles = SettingKey[Seq[File]]("axis-wsdl-files")
-    val axisPackageSpace = SettingKey[Option[String]]("axis-package-space", "Package to create Java files under, corresponds to -p / --package option in WSDL2Java")
-    val axisTimeout = SettingKey[Option[Int]]("axis-timeout","Timeout used when generating sources")
+    val axisPackageSpace = SettingKey[Option[String]](
+      "axis-package-space",
+      "Package to create Java files under, corresponds to -p / --package option in WSDL2Java"
+    )
+    val axisTimeout   = SettingKey[Option[Int]]("axis-timeout", "Timeout used when generating sources")
     val axisOtherArgs = SettingKey[Seq[String]]("axis-other-args", "Other arguments to pass to WSDL2Java")
-    val axisOutputDir = SettingKey[File]("axis-output-dir","Output directory for the sources")
+    val axisOutputDir = SettingKey[File]("axis-output-dir", "Output directory for the sources")
   }
 
   import autoImport._
@@ -33,41 +36,56 @@ object AxisPlugin extends sbt.AutoPlugin {
   override lazy val projectSettings: Seq[Setting[_]] =
     Seq(
       SbtAxis / javaSource := (Compile / sourceManaged).value,
-      axisWsdlFiles := Nil,
-      axisPackageSpace := None,
-      axisOtherArgs := Nil,
-      axisTimeout := Some(45),
-      axisOutputDir := sourceManaged.value,
-      axisWsdl2java := { (streams, axisWsdlFiles, SbtAxis/ javaSource, axisPackageSpace, axisTimeout,axisOutputDir,axisOtherArgs) map { runWsdlToJavas } }.value,
+      axisWsdlFiles        := Nil,
+      axisPackageSpace     := None,
+      axisOtherArgs        := Nil,
+      axisTimeout          := Some(45),
+      axisOutputDir        := sourceManaged.value,
+      axisWsdl2java := {
+        (streams,
+         axisWsdlFiles,
+         SbtAxis / javaSource,
+         axisPackageSpace,
+         axisTimeout,
+         axisOutputDir,
+         axisOtherArgs
+        ) map runWsdlToJavas
+      }.value,
       Compile / sourceGenerators += axisWsdl2java,
       Compile / managedSourceDirectories += (SbtAxis / javaSource).value,
       cleanFiles += (SbtAxis / javaSource).value,
       libraryDependencies ++= Seq(
-        "axis" % "axis" % "1.4",
-        "axis" % "axis-saaj" % "1.4",
-        "axis" % "axis-wsdl4j" % "1.5.1",
-        "javax.activation" % "activation" % "1.1.1",
-        "javax.mail" % "mail" % "1.4"
+        "axis"             % "axis"        % "1.4",
+        "axis"             % "axis-saaj"   % "1.4",
+        "axis"             % "axis-wsdl4j" % "1.5.1",
+        "javax.activation" % "activation"  % "1.1.1",
+        "javax.mail"       % "mail"        % "1.4"
       )
     )
 
-  private case class WSDL2JavaSettings(dest: File, packageSpace: Option[String], timeout:Option[Int],outputDir:File,otherArgs: Seq[String])
+  private case class WSDL2JavaSettings(dest: File,
+                                       packageSpace: Option[String],
+                                       timeout: Option[Int],
+                                       outputDir: File,
+                                       otherArgs: Seq[String]
+  )
 
-  private def runWsdlToJavas(
-    streams: TaskStreams,
-    wsdlFiles: Seq[File],
-    dest: File,
-    packageSpace: Option[String],
-    timeout: Option[Int],
-    outputDir: File,
-    otherArgs: Seq[String]): Seq[File] =
-    wsdlFiles.flatMap(wsdl =>
-      runWsImport(streams, wsdl, WSDL2JavaSettings(dest, packageSpace, timeout,outputDir,otherArgs))).distinct
+  private def runWsdlToJavas(streams: TaskStreams,
+                             wsdlFiles: Seq[File],
+                             dest: File,
+                             packageSpace: Option[String],
+                             timeout: Option[Int],
+                             outputDir: File,
+                             otherArgs: Seq[String]
+  ): Seq[File] =
+    wsdlFiles
+      .flatMap(wsdl => runWsImport(streams, wsdl, WSDL2JavaSettings(dest, packageSpace, timeout, outputDir, otherArgs)))
+      .distinct
 
   private def makeArgs(wsdlFile: File, settings: WSDL2JavaSettings): Seq[String] =
     settings.packageSpace.toSeq.flatMap(p => Seq("--package", p)) ++
-      Seq("-O",settings.timeout.map(_.toString).getOrElse("-1")) ++
-      Seq("-o",settings.outputDir.getCanonicalPath) ++
+      Seq("-O", settings.timeout.map(_.toString).getOrElse("-1")) ++
+      Seq("-o", settings.outputDir.getCanonicalPath) ++
       settings.otherArgs ++
       Seq(wsdlFile.getAbsolutePath)
 
@@ -79,8 +97,7 @@ object AxisPlugin extends sbt.AutoPlugin {
 
     val args = makeArgs(wsdlFile, settings)
     streams.log.debug("wsimport " + args.mkString(" "))
-    try
-      new WSDL2JavaWrapper().execute(args.toArray)
+    try new WSDL2JavaWrapper().execute(args.toArray)
     catch {
       case t: Throwable =>
         streams.log.error("Problem running WSDL2Java " + args.mkString(" "))
